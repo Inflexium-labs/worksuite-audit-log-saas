d6c25763bbcb3f38446a947c62fb0ea702ac1418
Hossain Mohammad Imran <hmimeee@gmail.com>
2021-07-29 00:53:02 +0600

    Removed member left sidebar

--- Resources/views/sections/member_left_sidebar.blade.php
+++
@@ -1,5 +0,0 @@
-<li>
-    <a href="{{route('admin.audit-log.index')}}" class="waves-effect">
-        <i class="fa fa-bus" aria-hidden="true"></i>
-        <span class="hide-menu">@lang('incident::app.title')</span></a>
-</li>
